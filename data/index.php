84acc69afd33560d717f8ecbffb9c634e6ca7d94
ybinarycat <vv.smirnov17@ya.ru>
2023-02-20 00:27:56 +0300

    ig

--- data/index.php
+++ data/index.php
@@ -1,5 +1,7 @@
 <h1> Вас приветствует Даронь-Кредит!</h1>
 
+<img src='glaz.png' width='600', height='200'>
+
 <form action="result.php" method="post">
  <p>Введите номер карты: <input type="text" name="card" /></p>
  <p><input type="submit" /></p>
@@ -9,3 +11,5 @@
 <p>Можешь даже невводить карту, тама будут пруфы, что всё робит</p>
 
 <h1>Сие детище стыдно выкладывать на гитхабы, но ладнаааа</h1>
+<h2>Я для приличия даже в докер запааковал уссё</h2>
+<h2> <a href="https://github.com/BinaryCat17/daron">Тыкк</a> </h2>
